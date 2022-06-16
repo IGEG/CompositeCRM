756131c197f115af21d6733c64e0fda18701f5b0
igor egorov <igor127@bk.ru>
2022-06-16 16:46:16 +0300

    Add project files.

---
+++ Controllers/InvoicesDebtController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompositeCRM.Controllers
+{
+    public class InvoicesDebtController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/InvoicesDebt.cs
@@ -0,0 +1,17 @@
+﻿namespace CompositeCRM.Models
+{
+    public class InvoicesDebt
+    {
+        public int Id { get; set; }
+        public string DepartmentNameDebt { get; set; }
+        public string DateInvoicesDebt { get; set; }
+        public int InvoiceNumberDebt { get; set; }
+        public string ClientNameDebt { get; set; }
+        public decimal InvoiceAmountDebt { get; set; }
+        public string EmployeeFullNameDebt { get; set; }
+        public int INNClientDebt { get; set; }
+        public string InvoiseStatusDebt { get; set; }
+
+
+    }
+}
