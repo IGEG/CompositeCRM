74147d65d01123fe7bbd1267eea2634eef39fb44
igor egorov <igor127@bk.ru>
2022-07-12 22:27:46 +0300

    invoicesPost

--- AngularUI/src/app/invoices-debt/post-invoices/post-invoices.component.ts
+++ AngularUI/src/app/invoices-debt/post-invoices/post-invoices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
 
 @Component({
   selector: 'app-post-invoices',
@@ -7,28 +8,61 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostInvoicesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpSevice: HttpService) { }
 
-  @Input() invDept:any;
-  DepartmentNameDebt?:string;
-  DateInvoicesDebt?:string;
-  InvoiceNumberDebt?:string;
-  ClientNameDebt?:string;
-  InvoiceAmountDebt?:string;
-  EmployeeFullNameDebt?:string;
-  INNClientDebt?:string;
-  InvoiseStatusDebt?:string;
+  @Input() invDept: any;
+
+  DepartmentNameDebt?: string;
+  DateInvoicesDebt?: string;
+  InvoiceNumberDebt?: string;
+  ClientNameDebt?: string;
+  InvoiceAmountDebt?: string;
+  EmployeeFullNameDebt?: string;
+  INNClientDebt?: string;
+  InvoiseStatusDebt?: string;
 
   ngOnInit(): void {
-    this.DepartmentNameDebt=this.invDept.DepartmentNameDebt;
-    this.DateInvoicesDebt=this.invDept.DateInvoicesDebt;
-    this.InvoiceNumberDebt=this.invDept.InvoiceNumberDebt;
-    this.ClientNameDebt=this.invDept.ClientNameDebt;
-    this.InvoiceAmountDebt=this.invDept.InvoiceAmountDebt;
-    this.EmployeeFullNameDebt=this.invDept.EmployeeFullNameDebt;
-    this.INNClientDebt=this.invDept.INNClientDebt;
-    this.InvoiseStatusDebt=this.invDept.InvoiseStatusDebt;
+    this.DepartmentNameDebt = this.invDept.DepartmentNameDebt;
+    this.DateInvoicesDebt = this.invDept.DateInvoicesDebt;
+    this.InvoiceNumberDebt = this.invDept.InvoiceNumberDebt;
+    this.ClientNameDebt = this.invDept.ClientNameDebt;
+    this.InvoiceAmountDebt = this.invDept.InvoiceAmountDebt;
+    this.EmployeeFullNameDebt = this.invDept.EmployeeFullNameDebt;
+    this.INNClientDebt = this.invDept.INNClientDebt;
+    this.InvoiseStatusDebt = this.invDept.InvoiseStatusDebt;
 
   }
 
+  addInvoicesDebt() {
+    var invoiceDebt =
+    {
+      DepartmentNameDebt: this.DepartmentNameDebt,
+      DateInvoicesDebt: this.DateInvoicesDebt,
+      InvoiceNumberDebt: this.InvoiceNumberDebt,
+      ClientNameDebt: this.ClientNameDebt,
+      InvoiceAmountDebt: this.InvoiceAmountDebt,
+      EmployeeFullNameDebt: this.EmployeeFullNameDebt,
+      INNClientDebt: this.INNClientDebt,
+      InvoiseStatusDebt: this.InvoiseStatusDebt
+    };
+    this.httpSevice.editInvoicesDebt(invoiceDebt).subscribe(data => { alert(data.toString()); });
+  }
+
+  changeInvoicesDebt() {
+    var invoiceDebt =
+    {
+      DepartmentNameDebt: this.DepartmentNameDebt,
+      DateInvoicesDebt: this.DateInvoicesDebt,
+      InvoiceNumberDebt: this.InvoiceNumberDebt,
+      ClientNameDebt: this.ClientNameDebt,
+      InvoiceAmountDebt: this.InvoiceAmountDebt,
+      EmployeeFullNameDebt: this.EmployeeFullNameDebt,
+      INNClientDebt: this.INNClientDebt,
+      InvoiseStatusDebt: this.InvoiseStatusDebt
+    };
+    this.httpSevice.changeInvoicesDebt(invoiceDebt).subscribe(data => { alert(data.toString()); });
+  }
+
 }
+
+
