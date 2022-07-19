96dcb3045043d010fdf5f467ccc6da4dd3b5e5bc
igor egorov <igor127@bk.ru>
2022-07-19 21:58:37 +0300

    new controller document

---
+++ Models/Document.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompositeCRM.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Clients { get; set; }
+        [Required]
+        public string TypeDoc { get; set; }
+        [Required]
+        public string DateOfAdded { get; set; }
+        [Required]
+        public string Employer { get; set; }
+ 
+    }
+}
--- Models/InvoicesDebt.cs
+++ Models/InvoicesDebt.cs
@@ -1,15 +1,25 @@
-﻿namespace CompositeCRM.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompositeCRM.Models
 {
     public class InvoicesDebt
     {
         public int Id { get; set; }
+        [Required]
         public string DepartmentNameDebt { get; set; }
+        [Required]
         public string DateInvoicesDebt { get; set; }
+        [Required]
         public int InvoiceNumberDebt { get; set; }
+        [Required]
         public string ClientNameDebt { get; set; }
+        [Required]
         public decimal InvoiceAmountDebt { get; set; }
+        [Required]
         public string EmployeeFullNameDebt { get; set; }
+        [Required]
         public int INNClientDebt { get; set; }
+        [Required]
         public string InvoiseStatusDebt { get; set; }
 
 
