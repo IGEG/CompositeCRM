0b5cd36bdefb94a535e13872e712d1cf2a7df099
igor egorov <igor127@bk.ru>
2022-07-20 22:23:44 +0300

    change

--- AngularUI/src/app/http.service.ts
+++ AngularUI/src/app/http.service.ts
@@ -11,6 +11,10 @@ export class HttpService {
 
  constructor( private http: HttpClient) { }
 
+
+ //InvoicesDebt (задолженности по счетам)//
+
+
  getAllInvoicesDebt():Observable<any[]>
  {
    return this.http.get<any>(this.url+"/InvoicesDebt");
@@ -18,22 +22,49 @@ export class HttpService {
 
  getOneInvoicesDebt(Id:number):Observable<any>
  {
-   return this.http.get(this.url+"InvoicesDebt"+"/"+Id);
+   return this.http.get(this.url+"/InvoicesDebt"+"/"+Id);
  }
 
  editInvoicesDebt(value:any)
  {
-   return this.http.post(this.url+"InvoicesDebt", value);
+   return this.http.post(this.url+"/InvoicesDebt", value);
  }
 
  changeInvoicesDebt(value:any)
  {
-   return this.http.put(this.url+"InvoicesDebt", value);
+   return this.http.put(this.url+"/InvoicesDebt", value);
  }
 
  deleteInvoicesDebt(value:any)
  {
-   return this.http.delete(this.url+"InvoicesDebt", value);
+   return this.http.delete(this.url+"/InvoicesDebt/"+ value);
+ }
+
+ //Documents (Вся канцелярия)//
+
+ getAllDocuments():Observable<any[]>
+ {
+   return this.http.get<any>(this.url+"/Document");
+ }
+
+ getOneDocument(Id:number):Observable<any>
+ {
+   return this.http.get(this.url+"/Document"+"/"+Id);
+ }
+
+ editDocument(value:any)
+ {
+   return this.http.post(this.url+"/Document", value);
+ }
+
+ changeDocument(value:any)
+ {
+   return this.http.put(this.url+"/Document", value);
+ }
+
+ deleteDocument(value:any)
+ {
+   return this.http.delete(this.url+"/Document/"+ value);
  }
 
 }
