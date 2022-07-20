5501e97eb2d03cc8bf222787fd131f2832b29553
igor egorov <igor127@bk.ru>
2022-07-20 21:25:24 +0300

    document component added

--- AngularUI/src/app/app.module.ts
+++ AngularUI/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvoicesDebtComponent } from './invoices-debt/invoices-debt.component';
@@ -10,13 +9,19 @@ import { HttpService } from './http.service';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DocumentComponent } from './document/document.component';
+import { GetDocumentComponent } from './document/get-document/get-document.component';
+import { PostDocumentComponent } from './document/post-document/post-document.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InvoicesDebtComponent,
     GetInvoicesComponent,
-    PostInvoicesComponent
+    PostInvoicesComponent,
+    DocumentComponent,
+    GetDocumentComponent,
+    PostDocumentComponent
   ],
   imports: [
     BrowserModule,
