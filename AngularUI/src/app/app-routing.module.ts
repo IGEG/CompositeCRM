9a58a74092a76424fbe25bcf1380049b5eea6c09
igor egorov <igor127@bk.ru>
2022-07-20 22:23:22 +0300

    httpservice changed

--- AngularUI/src/app/app-routing.module.ts
+++ AngularUI/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvoicesDebtComponent } from './invoices-debt/invoices-debt.component';
+import { DocumentComponent } from './document/document.component';
 
 const routes: Routes = [
-  {
-    path:"InvoicesDebt",component:InvoicesDebtComponent
-  }
+  {path:"InvoicesDebt",component:InvoicesDebtComponent},
+  {path:"Document",component:DocumentComponent}
+
 ];
 
 @NgModule({
